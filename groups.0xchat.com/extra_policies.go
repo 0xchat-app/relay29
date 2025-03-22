a171f981b23e290d17c41b2479f73b8e62c5164b
w <130329555+wcat7@users.noreply.github.com>
2025-03-22 23:11:34 +0800

    remove ratelimit

--- groups.0xchat.com/extra_policies.go
+++ groups.0xchat.com/extra_policies.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"time"
 
 	"github.com/fiatjaf/relay29"
 	"github.com/nbd-wtf/go-nostr"
@@ -23,21 +22,21 @@ func blockDeletesOfOldMessages(ctx context.Context, target, deletion *nostr.Even
 // very strict rate limits
 var rateLimitBuckets = xsync.NewMapOf[*relay29.Group, *rate.Limiter]()
 
-func rateLimit(ctx context.Context, event *nostr.Event) (reject bool, msg string) {
-	group := state.GetGroupFromEvent(event)
-	if group == nil && event.Kind != nostr.KindSimpleGroupCreateGroup {
-		return true, "invalid group"
-	}
+// func rateLimit(ctx context.Context, event *nostr.Event) (reject bool, msg string) {
+// 	group := state.GetGroupFromEvent(event)
+// 	if group == nil && event.Kind != nostr.KindSimpleGroupCreateGroup {
+// 		return true, "invalid group"
+// 	}
 
-	bucket, _ := rateLimitBuckets.LoadOrCompute(group, func() *rate.Limiter {
-		return rate.NewLimiter(rate.Every(time.Minute*2), 30)
-	})
+// 	bucket, _ := rateLimitBuckets.LoadOrCompute(group, func() *rate.Limiter {
+// 		return rate.NewLimiter(rate.Every(time.Minute*2), 30)
+// 	})
 
-	if rsv := bucket.Reserve(); rsv.Delay() != 0 {
-		rsv.Cancel()
-		return true, "rate-limited"
-	} else {
-		rsv.OK()
-		return false, ""
-	}
-}
+// 	if rsv := bucket.Reserve(); rsv.Delay() != 0 {
+// 		rsv.Cancel()
+// 		return true, "rate-limited"
+// 	} else {
+// 		rsv.OK()
+// 		return false, ""
+// 	}
+// }
